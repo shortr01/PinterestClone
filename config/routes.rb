63335baf82543864a6f99fc53357352ae5e23efa
Robyn Shorter <robynshorter@gmail.com>
2016-03-07 22:24:41 +0000

    before imagaemagick

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  
+
+  resources :pins
+  resources :pins
+  resources :pins
   root 'home#index'
   get 'home/about'
-
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
